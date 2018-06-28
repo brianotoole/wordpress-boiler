a898e8dc1763468207f0eb9dbe5fd6af8820da28
Brian O'Toole <brian@sparxoo.com>
2018-06-28 11:23:00 -0400

    add local acf-json folder for control over fields

---
+++ acf-json/index.php
@@ -0,0 +1,5 @@
+<?php
+// Local ACF-JSON allows for version control over your field settings
+// File is automatically generated upon saving a field group
+// Best practices: //www.advancedcustomfields.com/resources/local-json/
+?>
