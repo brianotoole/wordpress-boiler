06e683f37f80e6fa938f2a84f0f4a39bfc72d353
Brian O'Toole <brian@sparxoo.com>
2018-06-28 22:16:50 -0400

    prepend slug_name modifier to hero component

--- template-parts/hero.php
+++ template-parts/hero.php
@@ -1,10 +1,14 @@
-<?php
+<?php // HERO
+
 // Custom Field Group == Component: Hero
-//$hero_title = get_field('home_hero_title');
-//$hero_subheading = get_field('home_hero_subheading');
-//$hero_button = get_field('home_hero_button');
+$hero_title= get_field('hero_title');
+$hero_subtitle= get_field('home_hero_subtitle');
+$hero_button = get_field('home_hero_button');
+// get slug name via post_name property
+$slug_name = get_post_field('post_name', get_post());
 ?>
-<section class="hero">
+
+<section class="hero hero--<?php echo $slug_name; ?>">
   <div class="hero__content container">
       <h1 class="hero__title">WP Theme Boilerplate</h1>
       <p class="hero__subtitle">A minimalistic wordpress theme that follows BEM methodology and comes preloaded with a few resusable components and design considerations that will help save time with initial setup.</p>
@@ -13,15 +17,3 @@
       </div>
   </div>
 </section>
-
-<!-- MODAL -->
-<div class="modal">
-  <div class="modal__inner">
-    <div class="container container--narrow">
-      <h2 class="modal__title">A Modal</h2>
-      <p class="modal__description">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Excepturi fugit amet reprehenderit in ut quae ad aspernatur ipsa quasi atque qui, rem ratione, provident dignissimos fugiat delectus odio non a?</p>
-    </div><!--/.container-->
-  </div><!--/.modal__inner-->
-  <div class="modal__close">X</div>
-</div><!--/.modal-->
-<!-- /MODAL -->
