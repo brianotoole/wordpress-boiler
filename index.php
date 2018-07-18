775b6c5b073944c62f911c9bd89ee70185e2ab61
Brian O'Toole <botoole518@gmail.com>
2018-07-18 11:32:44 -0400

    build out content / content-none

--- index.php
+++ index.php
@@ -6,8 +6,15 @@ get_header();
 // Hero
 get_template_part('template-parts/hero', '');
 
-// Content
-get_template_part('template-parts/content', '');
+if ( have_posts() ) :
+  while ( have_posts() ) : the_post(); 
+  // Content -- Default (Basic Loop)
+  get_template_part('template-parts/content', '');
+  endwhile;
+  else : 
+  // Content -- None (404)
+  get_template_part('template-parts/content', 'none');
+endif;
 
 // Footer
 get_footer(); 
---
+++ template-parts/content-none.php
@@ -0,0 +1,12 @@
+<?php // CONTENT-NONE == 404 ?>
+
+<section class="section section--not-found">
+
+<div class="container">
+  <div class="content">
+    <h1 class="heading"><?php _e( 'Page Not Found - 404', 'spx' ); ?></h1>
+    <p>We cannot find the page you are looking for. <a href="<?php echo home_url(); ?>">Click here to return home</a>.</p>
+  </div><!--/.content-->
+</div><!--/.container-->
+
+</section>
